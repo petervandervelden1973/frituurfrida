670e88dfcd7a3805445feb20379ee879c717f9fa
Peter van der Velden <petervandervelden@hotmail.com>
2017-05-08 15:53:11 +0200

    Einde 8 mei 2017

---
+++ frituurfrida/src/be/vdab/repositories/SausRepository.java
@@ -0,0 +1,29 @@
+package be.vdab.repositories;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import be.vdab.entities.Saus;
+
+public class SausRepository {
+	private static final Map<Long,Saus> SAUZEN = new ConcurrentHashMap<>();
+	static {
+		SAUZEN.put(3L, new Saus(3, "cocktail",
+				Arrays.asList("mayonaise", "ketchup", "cognac")));
+		SAUZEN.put(6L, new Saus(6, "mayonaise",
+				Arrays.asList("ei", "mosterd")));
+		SAUZEN.put(7L, new Saus(7, "mosterd",
+				Arrays.asList("mosterd", "azijn", "witte wijn")));
+		SAUZEN.put(12L, new Saus(12, "tartare",
+				Arrays.asList("mayonaise", "augurk", "tabasco")));
+		SAUZEN.put(44L, new Saus(44, "vinaigrette",
+				Arrays.asList("olijfolie", "mosterd", "azijn")));
+	}
+	
+	public List<Saus> findAll() {
+		return new ArrayList(SAUZEN.values());
+	}
+}
--- frituurfrida/src/be/vdab/servlets/SauzenServlet.java
+++ frituurfrida/src/be/vdab/servlets/SauzenServlet.java
@@ -1,8 +1,6 @@
 package be.vdab.servlets;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,21 +8,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import be.vdab.entities.Saus;
+import be.vdab.repositories.SausRepository;
 
 @WebServlet("/sauzen.htm")
-public class SauzenServlet extends HttpServlet {
+	public class SauzenServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String VIEW = "/WEB-INF/JSP/sauzen.jsp";
+	private final SausRepository sausRepository = new SausRepository();
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 		throws ServletException, IOException {
-		List<Saus> sauzen = Arrays.asList(
-			new Saus(3L, "cocktail", Arrays.asList("mayonaise", "ketchup", "cognac")),
-			new Saus(6L, "mayonaise", Arrays.asList("ei", "plantaardige olie", "mosterd", "azijn")),
-			new Saus(7L, "mosterd", Arrays.asList("mosterd", "azijn", "witte wijn")),
-			new Saus(12L, "tartare", Arrays.asList("mayonaise", "augurk", "tobasco")),
-			new Saus(44L,"vinaigrette",Arrays.asList("olijfolie","mosterd","azijn")));
-	request.setAttribute("sauzen", sauzen);
-	request.getRequestDispatcher(VIEW).forward(request, response);
+		request.setAttribute("sauzen", sausRepository.findAll());
+		request.getRequestDispatcher(VIEW).forward(request, response);
 	}
 }
